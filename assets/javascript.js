8aa4ccf1c4468d415a1e51ae1031d2115f77783f
Michael Pesce <Michael Pesce>
2017-05-15 19:09:41 -0400

    js page no longer has errors

--- assets/javascript.js
+++ assets/javascript.js
@@ -15,7 +15,51 @@ firebase.initializeApp(config);
 
 //global variables
 var artist = "";
+var artisyID;
 var database = firebase.database();
+
+function getArtist(){
+  var name = ("#inputForm").val();
+
+  var buildURL1 = "https://api.spotify.com/v1/search?q=" + name
+   + "&type=artist";
+
+
+  $.ajax({
+     
+        url: buildURL1,
+        method: "GET"
+            })
+       //stops ajax call and retreives response
+       .done(function(response){
+        
+        //sets variable to data of the response
+        console.log(response.artists.items[0].name, 
+          response.artists.items[0].id);
+})
+
+function getTopTracks(){
+
+
+  var buildURL2 = "https://api.spotify.com/v1/search?q=prince&type=artist";
+
+      
+
+     $.ajax({
+     
+        url: buildURL2,
+        method: "GET"
+            })
+       //stops ajax call and retreives response
+       .done(function(response){
+        
+        //sets variable to data of the response
+        console.log(response.artists.items[0].name, 
+          response.artists.items[0].id);
+        
+
+})
+
 //starts on click function to enter earch term
 $("#inputForm").on("click", function(event) {
      
@@ -23,10 +67,13 @@ $("#inputForm").on("click", function(event) {
      
      //pushes search term into database
      artistName = $("#inputForm").val().trim();
+
      
      database.ref().push({
      
         "artist": artistName
+        //need to create an artist ID var to push 
+        //artist object id to database
      
         });
 
@@ -45,8 +92,9 @@ $("#inputForm").on("click", function(event) {
      
      artist = $(this).attr("data-name");
      
-     var queryURL = "https://musicbrainz.org/ws/2/" 
-     + artist + "<MBID>?inc=<INC>";
+     var queryURL = "https://api.spotify.com/v1/search?q=prince&type=artist";
+
+      
 
      $.ajax({
      
@@ -57,12 +105,14 @@ $("#inputForm").on("click", function(event) {
        .done(function(response){
        	
        	//sets variable to data of the response
-       	var result =response.data;
-       	console.log(response);
-       	console.log("hello");
+       	console.log(response.artists.items[0].name, 
+          response.artists.items[0].id);
+       	
 
 
 
-       })
-
- })
+       });
+})
+ 
+}
+}
