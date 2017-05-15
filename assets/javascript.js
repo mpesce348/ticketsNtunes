198eeea9b417a1acafe6f11b2acac2c1c702b031
Jhemarr Anderson <jhemarranderson@Jhemarrs-MacBook-Pro.local>
2017-05-15 19:57:02 -0400

    Wiki API somewhat working

--- assets/javascript.js
+++ assets/javascript.js
@@ -15,19 +15,91 @@ $(document).ready(function() {
 
   //global variables
   var artist = "";
+  var artistID;
   var database = firebase.database();
-  //starts on click function to enter earch term
-  $("#inputForm").on("click", function(event) {
-     
-     event.preventDefault();
-     
-     //pushes search term into database
-     artistName = $("#inputForm").val().trim();
-     
-     database.ref().push({
-     
+
+function getArtist() {
+    var name = ("#inputForm").val();
+
+    var buildURL1 = "https://api.spotify.com/v1/search?q=" + name +
+        "&type=artist";
+
+    $.ajax({
+             url: buildURL1,
+            method: "GET"
+        })
+        //stops ajax call and retreives response
+        .done(function(response) {
+
+            //sets variable to data of the response
+            console.log(response.artists.items[0].name,
+                response.artists.items[0].id);
+        })
+    }
+
+function getTopTracks() {
+
+    var buildURL2 = "https://api.spotify.com/v1/search?q=prince&type=artist";
+
+    $.ajax({
+
+            url: buildURL2,
+            method: "GET"
+        })
+        //stops ajax call and retreives response
+        .done(function(response) {
+
+            //sets variable to data of the response
+            console.log(response.artists.items[0].name,
+                response.artists.items[0].id);
+        })
+}
+
+//starts on click function to enter earch term
+$("#inputForm").on("click", function(event) {
+
+    event.preventDefault();
+    //pushes search term into database
+    artistName = $("#inputForm").val().trim();
+
+    database.ref().push({
+
+
         "artist": artistName
-     
+            //need to create an artist ID var to push 
+            //artist object id to database
+
+    });
+
+    //set up function to take snapshot of 
+    //database value on any value change
+
+    database.ref().on("value", function(snapshot) {
+        var snapshotVal = snapshot.val();
+        console.log(snapshotVal);
+        var valKeys = Object.keys(snapshotVal);
+        console.log(valKeys);
+    })
+
+    // set up api key
+    // perform call to get info from api
+
+    artist = $(this).attr("data-name");
+
+    var queryURL = "https://api.spotify.com/v1/search?q=prince&type=artist";
+
+    $.ajax({
+
+            url: queryURL,
+            method: "GET"
+        })
+        //stops ajax call and retreives response
+        .done(function(response) {
+
+            //sets variable to data of the response
+            console.log(response.artists.items[0].name,
+                response.artists.items[0].id);
+
         });
 
      //set up function to take snapshot of 
@@ -61,8 +133,6 @@ $(document).ready(function() {
           console.log(response);
           console.log("hello");
 
-
-
        });
     
 
@@ -92,4 +162,8 @@ $(document).ready(function() {
             $(".container").text(JSON.stringify(response));
           });
 
+    getArtist();
+    getTopTracks();
+
         });
+
