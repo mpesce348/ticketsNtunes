e17e65a6cb61055df97f62cb6101bc9c67014d9e
Jhemarr Anderson <jhemarranderson@Jhemarrs-MBP.fios-router.home>
2017-05-19 20:12:29 -0400

    removed unnecessary code

--- assets/javascript.js
+++ assets/javascript.js
@@ -39,13 +39,6 @@ $(document).ready(function() {
             var genre = response.artists.items[0].genres;
 
 
-     $.ajax({
-      url: queryURL,
-      method: "GET"
-     })
-
-
-
 
             // Building a SECOND URL to query another Spotify endpoint (this one for the tracks)
             var queryURLTracks = "https://api.spotify.com/v1/artists/" + artistID + "/top-tracks?country=US";
@@ -60,8 +53,6 @@ $(document).ready(function() {
                 //creates variable to seach api for artist's top track data 
                 var queryURLTracks1 = "https://api.spotify.com/v1/artists/" + artistID + "/top-tracks?country=US";
 
-      //RETRIEVES SPOTIFY INFO ON ALBUMS
-      var queryURLTracks = "https://api.spotify.com/v1/artists/"+ artistID +"/top-tracks?country=US";
                 // Running a second AJAX call to get the tracks associated with that Spotify id
                 $.ajax({
                     url: queryURLTracks1,
@@ -95,31 +86,6 @@ $(document).ready(function() {
         });
     };
 
-        // Logging the tracks
-        // console.log(trackResponse.tracks[0].name);
-        // console.log(trackResponse.tracks[1].name);
-        var trackPanel  = $('#trackData')
-        var trackTop3List = $('<ol>')
-        for(var i = 0; i < 10; i++){
-          var trackListItem = $('<li>')
-          var trackHeader = $('<h3>')
-          console.log(trackResponse.tracks[i].name);
-          trackHeader.text(trackResponse.tracks[i].name)
-          trackListItem.append(trackHeader) 
-          trackTop3List.append(trackListItem);
-        }
-
-        trackPanel.append(trackTop3List)
-        // Appending the new player into the HTML
-        // $("#topTrack1").text(JSON.stringify(trackResponse.tracks[0].name));
-        // $("#topTrack2").text(JSON.stringify(trackResponse.tracks[1].name));
-        // $("#topTrack3").text(JSON.stringify(trackResponse.tracks[2].name));
-        
-      });
-   // });
-  //});
-//})
-  
     //WIKI API
     function getWiki() {
 
@@ -127,12 +93,9 @@ $(document).ready(function() {
         //creates variable wiki and defines it as the text input from the input form
         var wiki = $("#inputForm").val();
 
-
         //builds query url using text from the input form
         var queryURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=" + wiki + "&exintro=1&explaintext=1";
 
-
-        console.log();
 
         //ajax call to retreive wiki data for text description
         $.ajax({
@@ -140,7 +103,6 @@ $(document).ready(function() {
             method: "GET",
             cors: true,
             dataType: "jsonp"
-
         //completes ajax call and creates function with response data
         }).done(function(response) {
             console.log(response);
@@ -149,25 +111,6 @@ $(document).ready(function() {
             //sets the text in the div with id "wikiData" to a stringified version of 
             //the text pulled from the wiki api object
             $("#wikiData").text(JSON.stringify(response.query.pages[4429395].extract));
-
-            //builds additional search query ur; to look seach for images 
-            var queryURL2 = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=" + wiki + "&prop=images=1";
-
-
-
-            $.ajax({
-                url: queryURL2,
-                method: "GET",
-                cors: true,
-                dataType: "jsonp"
-            }).done(function(response) {
-                console.log(response);
-                console.log(response.query.pages);
-                
-
-            })
-
-
         })
 
     }
@@ -181,16 +124,12 @@ $(document).ready(function() {
     $("#inputBtn").on("click", function(event) {
 
 
-
-
         event.preventDefault();
         console.log("working click")
         $("#trackData").empty();
         $("#wikiData").empty();
         //pushes search term into database
         var artistName = $("#inputForm").val().trim();
-
-
 
 
         database.ref().push({
@@ -219,8 +158,6 @@ $(document).ready(function() {
         console.log(getWiki);
     })
 
-    getWiki();
-
- 
 
 
+});
